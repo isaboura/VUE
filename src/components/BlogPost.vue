ff3220bc155e8ca57e13ed636477137cd1b1608a
isaboura <isaboura@iesch.org>
2025-05-27 17:36:59 +0200

    segundos componentes

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,24 @@
+<script setup>
+//defineProps(['titulo', 'id', 'contenido'])
+
+defineProps({
+    titulo: String,
+    id : Number,
+    contenido :{
+        type : String, 
+        default : 'Sin descripcion'
+    }
+})
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{ id }}  -    {{ titulo }}</h5>
+            <p>{{ contenido }}</p>
+        </div>
+    </div>
+</template>
+
+<style></style>
---
+++ src/components/BotonContador.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { ref } from 'vue';
+    const contador = ref(0);
+    const incrementar = () => {
+        contador.value ++;
+    }
+</script>
+
+<template>
+    <button @click="incrementar()" class="btn btn-primary" >Contador {{ contador }}</button>
+    
+</template>
+
+<style>
+</style>
